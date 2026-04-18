b38047cfdde8b3803ccb7f4b65ffbd680f1103a3
kar5303 <kar5303@gmail.com>
2026-04-18 17:12:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -148,4 +148,4 @@ def stream_mp3():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False) 
